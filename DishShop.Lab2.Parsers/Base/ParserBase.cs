87096be574de014c93803cab7ec6e187b949355e
maxKutvitzky <baznalt@gmail.com>
2022-12-25 04:17:01 +0200

    Created basic API

--- DishShop.Lab2.Parsers/Base/ParserBase.cs
+++ DishShop.Lab2.Parsers/Base/ParserBase.cs
@@ -5,7 +5,7 @@ namespace DishShop.Lab2.Parsers.Base;
 
 public abstract class ParserBase
 {
-    protected const string DishSchemaPath =
+    public const string DishSchemaPath =
         @"D:\UNIVER\3 Course\ITROI\Project\DishShop\DishShop.Lab1.XML\XML\Dish.xsd";
 
     protected ParserBase(IDishShopMarshalUtils documentHandler, IDishShopMarshalMapper<Dish> dishMapper)
@@ -14,6 +14,6 @@ public abstract class ParserBase
         DishMapper = dishMapper;
     }
 
-    protected IDishShopMarshalMapper<Dish> DishMapper { get; }
+    public IDishShopMarshalMapper<Dish> DishMapper { get; }
     public IDishShopMarshalUtils DocumentHandler { get; }
 }
---
+++ DishShop.Lab3-4.API/Utils/PathUtils.cs
@@ -0,0 +1,9 @@
+﻿namespace DishShop.Lab3_4.API.Utils
+{
+    public static class PathUtils
+    {
+        public static readonly string internalDishesPath = @"D:\UNIVER\3 Course\ITROI\Project\DishShop\DishShop.Lab1.XML\XML\Dishes.xml";
+        public static readonly string internalDishPath = @"D:\UNIVER\3 Course\ITROI\Project\DishShop\DishShop.Lab1.XML\XML\Dish.xml";
+        public static readonly string internalStorageDirectory = @"D:\UNIVER\3 Course\ITROI\Project\DishShop\DishShop.Lab1.XML\XML\";
+    }
+}
